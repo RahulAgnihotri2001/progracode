d3f9732d43b1c1ce82910f779cb0c643da37bc18
RahulAgnihotri2001 <100308406+RahulAgnihotri2001@users.noreply.github.com>
2023-06-28 01:43:10 +0530

    Added solution - LeetHub

---
+++ Longest Prefix Suffix - GFG/longest-prefix-suffix.cpp
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+//Initial template for C++ 
+ 
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+//User function template for C++
+
+class Solution{
+  public:		
+	int lps(string s) {
+	    // Your code goes here
+	    int n=s.length();
+	    vector<int> vec(n,0);
+	    for(int i=1;i<n;i++){
+	        int j=vec[i-1];
+	        while(j>0 && s[i]!=s[j]){
+	            j=vec[j-1];
+	        }
+	        if(s[i]==s[j]){
+	            j++;
+	        }
+	        vec[i]=j;
+	    }
+	    /*
+	    for(int i=0;i<n;i++){
+	        cout<<vec[i]<<endl;
+	    }
+	    */
+	    return vec[n-1];
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() 
+{
+   
+
+   	ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+   
+   	int t;
+   	cin >> t;
+   	while(t--)
+   	{
+   		string str;
+   		cin >> str;
+
+   		Solution ob;
+
+   		cout << ob.lps(str) << "\n";
+   	}
+
+    return 0;
+}
+
+// } Driver Code Ends
